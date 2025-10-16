63dfb7cc28a4d53efbbed54961ef27ce10ac60ba
Hugo Seiji Watanabe <hugo_seiji@hotmail.com>
2025-10-16 16:18:45 -0300

    project src

---
+++ src/main/java/com/hacka/wstats/WstatsServerAppApplication.java
@@ -0,0 +1,13 @@
+package com.hacka.wstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WstatsServerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WstatsServerAppApplication.class, args);
+	}
+
+}
